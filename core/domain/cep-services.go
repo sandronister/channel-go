e4cad67240e1eef2cb87c4ae967946c0403ed109
Julio Sandroni <sandronister@gmail.com>
2023-10-13 09:42:49 -0300

    remove struct not used

--- core/domain/cep-services.go
+++ core/domain/cep-services.go
@@ -5,17 +5,6 @@ import (
 	"net/http"
 )
 
-type CDNResult struct {
-	Code       string `json:"code"`
-	State      string `json:"state"`
-	City       string `json:"city"`
-	District   string `json:"district"`
-	Address    string `json:"address"`
-	Status     int    `json:"status"`
-	Ok         bool   `json:"ok"`
-	StatusText string `json:"statusText"`
-}
-
 type CepServices interface {
 	Get(ctx context.Context, data chan<- string, cep string)
 }
